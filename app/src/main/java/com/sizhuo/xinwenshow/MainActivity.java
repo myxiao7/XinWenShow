7aaf9d3b55993232da2b9f5b43864e1a9175dcbf
My灬xiao7 <494643819@qq.com>
2015-12-09 17:14:49 +0800

    UIL图片加载类5

--- app/src/main/java/com/sizhuo/xinwenshow/MainActivity.java
+++ app/src/main/java/com/sizhuo/xinwenshow/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -15,19 +16,30 @@ import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.sizhuo.xinwenshow.adapter.MyFragPageAdapter;
+import com.sizhuo.xinwenshow.fragment.Frag01;
 import com.sizhuo.xinwenshow.util.StatusBar;
 import com.sizhuo.xinwenshow.util.SystemBarTintManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;//标题栏
     private TabLayout tabLayout;//导航栏
     private ViewPager viewPager;
+    private String[] titles = {"热点","体育","烟台","视频","其他"};
+    private List<Fragment> fragments = new ArrayList<Fragment>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initViews();
+        MyFragPageAdapter myFragPageAdapter = new MyFragPageAdapter(getSupportFragmentManager(),fragments,titles);
+        viewPager.setAdapter(myFragPageAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabsFromPagerAdapter(myFragPageAdapter);
     }
 
     private void initViews() {
@@ -37,13 +49,16 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         tabLayout = (TabLayout) findViewById(R.id.main_tablayout);
         viewPager = (ViewPager) findViewById(R.id.main_viewpager);
-        tabLayout.addTab(tabLayout.newTab().setText("热点"));
+        /*tabLayout.addTab(tabLayout.newTab().setText("热点"));
         tabLayout.addTab(tabLayout.newTab().setText("体育"));
         tabLayout.addTab(tabLayout.newTab().setText("烟台"));
         tabLayout.addTab(tabLayout.newTab().setText("财经"));
         tabLayout.addTab(tabLayout.newTab().setText("视频"));
-        tabLayout.addTab(tabLayout.newTab().setText("其他"));
-
+        tabLayout.addTab(tabLayout.newTab().setText("其他"));*/
+        for (int i = 0; i < 5; i++) {
+            Fragment frag01 = new Frag01();
+            fragments.add(frag01);
+        }
     }
 
 
